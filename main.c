9997d04a50300f094eb3a883952b6d2753cf8ae7
chiran-christian <66570028+chiran-christian@users.noreply.github.com>
2025-03-11 17:41:59 +0200

    lab4

--- main.c
+++ main.c
@@ -1,92 +1,245 @@
-#include <stdio.h>
+/* WiFi station Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
-#define GPIO_OUTPUT_IO 4
-#define GPIO_OUTPUT_PIN_SEL (1ULL<<GPIO_OUTPUT_IO)
-#define GPIO_BUTTON_IO 2
-#define GPIO_BUTTON_PIN_SEL (1ULL<<GPIO_BUTTON_IO)
-void vTask(void * pvParameters)
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "D:\scs_2A\lab4\lab4\mdns\include\mdns.h"
+#include "esp_err.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#include "lwip/sockets.h"
+#include "lwip/netdb.h"
+
+#define CONFIG_ESP_WIFI_SSID      "lab-iot"
+#define CONFIG_ESP_WIFI_PASS      "IoT-IoT-IoT"
+#define CONFIG_ESP_MAXIMUM_RETRY  5
+#define CONFIG_LOCAL_PORT         10001
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
 {
-    gpio_config_t io_conf = {};
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //bit mask of the pins that you want to set
-    io_conf.pin_bit_mask = GPIO_BUTTON_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
-
-    static int button_count = 0;
-    int last_button = 1;
-    while (1) {
-        int current_button = gpio_get_level(GPIO_BUTTON_IO);
-        if (last_button ==1 && current_button ==0)
-        {
-            button_count++;
-            printf("Buton apasat %d\n", button_count);
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
         }
-        last_button = current_button;
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     }
 }
 
-void app_main() {
-    //zero-initialize the config structure.
-    gpio_config_t io_conf = {};
-    //disable interrupt
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    //set as output mode
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    //bit mask of the pins that you want to set
-    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
-    //disable pull-down mode
-    io_conf.pull_down_en = 0;
-    //disable pull-up mode
-    io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
-    gpio_config(&io_conf);
-    
-    int cnt = 0;
-    while(1) {
-        printf("cnt: %d\n", cnt++);
-        if(cnt % 4 == 0)
-        {
-            gpio_set_level(GPIO_OUTPUT_IO, 1);
-            vTaskDelay(750 / portTICK_PERIOD_MS);
+void resolve_mdns_host(const char * host_name)
+{
+    printf("Query A: %s.local", host_name);
+
+    esp_ip4_addr_t addr;
+    addr.addr = 0;
+
+    esp_err_t err = mdns_query_a(host_name, 2000,  &addr);
+    if(err){
+        if(err == ESP_ERR_NOT_FOUND){
+            printf("Host was not found!");
+            return;
         }
-        if(cnt % 4 == 1)
-        {
-            gpio_set_level(GPIO_OUTPUT_IO, 0);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        if(cnt % 4 == 2)
-        {
-            gpio_set_level(GPIO_OUTPUT_IO, 1);
-            vTaskDelay(500 / portTICK_PERIOD_MS);
-        }
-        if(cnt % 4 == 3)
-        {
-            gpio_set_level(GPIO_OUTPUT_IO, 0);
-            vTaskDelay(250 / portTICK_PERIOD_MS);
-        }
+        printf("Query Failed");
+        return;
     }
+
+    printf(IPSTR, IP2STR(&addr));
 }
 
-/* 
-    Ce rol are functia gpio config?
-gpio_config are rol de a configura GPIO4 ca si port de iesire. 
+bool wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
 
-    In codul exemplu, pinul GPIO4 este configurat ca iesire. Care sunt celelalte
-moduri ın care poate fi configurat un pin GPIO?
-GPIO4 poate fi configurat ca si pull-up/pull-down, iesire/intrare, pin mapping
+    ESP_ERROR_CHECK(esp_netif_init());
 
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
-    Explicati apelul vTaskDelay.
-apelul functii vTaskDelay este pentru a creea o pauza pentru a observa cand ledul este activ si inactiv. 
-    De ce functia principala se numeste app main?
-*/
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .password = CONFIG_ESP_WIFI_PASS,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASS);
+        return true;
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+    esp_err_t err = mdns_init();
+    if (err) {
+        printf("MDNS Init failed: %d\n", err);
+    }
+    printf("Aici");
+    mdns_hostname_set("esp32-Chiran");
+    return false;
+}
+
+static void udp_task(void *pvParameters)
+{
+    char rx_buffer[128];
+    char addr_str[128];
+    int addr_family = 0;
+    int ip_protocol = 0;
+
+    /*
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr(CONFIG_PEER_IP_ADDR); // unde #define CONFIG_PEER_IP_ADDR "192.168.89.abc"
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(CONFIG_PEER_PORT);
+    addr_family = AF_INET;
+    ip_protocol = IPPROTO_IP;
+    */
+
+    struct sockaddr_in local_addr;
+    local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(CONFIG_LOCAL_PORT);
+    ip_protocol = IPPROTO_IP;
+    addr_family = AF_INET;
+
+    while(1)
+    {
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created");
+
+        int err = bind(sock, (struct sockaddr *)&local_addr, sizeof(local_addr));
+        if (err < 0) {
+            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        }
+        ESP_LOGI(TAG, "Socket bound, port %d", CONFIG_LOCAL_PORT);
+
+        while (1) {
+
+            struct sockaddr source_addr;
+            socklen_t socklen = sizeof(source_addr);
+            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, &source_addr, &socklen);
+
+            // Error occurred during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                break;
+            }
+            // Data received
+            else {
+                 inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+                ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+                ESP_LOGI(TAG, "%s", rx_buffer);
+            }
+
+            vTaskDelay(200 / portTICK_PERIOD_MS);
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    bool connected = wifi_init_sta();
+    esp_err_t err = mdns_init();
+    if (err) {
+        printf("MDNS Init failed: %d\n", err);
+    }
+    printf("Aici\n");
+    mdns_hostname_set("esp32-Chiran");
+    resolve_mdns_host("esp32-Chiriac");
+    if (connected) {
+        xTaskCreate(udp_task, "udp_task", 4096, NULL, 5, NULL);
+    }
+}
